db5a76fe803d5a9d3f6ffbfa7bbf6185cd2644b0
ha-spring <spring4boot@gmail.com>
2023-07-26 08:28:35 +0000

    add fees receiver

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -6,9 +6,11 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Airdrop is Ownable {
     uint256 public fee;
+    address public feesReceiver;
 
-    constructor(uint256 initFee) {
+    constructor(uint256 initFee, address initFeesReceiver) {
         fee = initFee;
+        feesReceiver = initFeesReceiver;
     }
 
     function airdrop(
@@ -20,14 +22,36 @@ contract Airdrop is Ownable {
         require(msg.value == fee, "Insufficient fee");
 
         for (uint256 i = 0; i < _recipients.length; i++) {
-            require(IERC20(_tokenAddress).balanceOf(msg.sender) >= _quantities[i], "Insufficient balance");
-            require(IERC20(_tokenAddress).allowance(msg.sender, address(this)) >= _quantities[i], "Insufficient allowance");
+            require(
+                IERC20(_tokenAddress).balanceOf(msg.sender) >= _quantities[i],
+                "Insufficient balance"
+            );
+            require(
+                IERC20(_tokenAddress).allowance(msg.sender, address(this)) >=
+                    _quantities[i],
+                "Insufficient allowance"
+            );
 
-            IERC20(_tokenAddress).transferFrom(msg.sender, _recipients[i], _quantities[i]);
+            IERC20(_tokenAddress).transferFrom(
+                msg.sender,
+                _recipients[i],
+                _quantities[i]
+            );
+        }
+
+        payable(feesReceiver).transfer(fee);
+
+        if (msg.value > fee) {
+            payable(msg.sender).transfer(msg.value - fee);
         }
     }
 
     function updateFee(uint256 newFee) external onlyOwner {
         fee = newFee;
+    }
+
+    function setFeesReceiver(address receiver) external onlyOwner {
+        require(receiver != address(0), "Invalid receiver address");
+        feesReceiver = receiver;
     }
 }
--- test/airdrop.ts
+++ test/airdrop.ts
@@ -8,100 +8,172 @@ describe("Airdrop", function () {
   let owner;
   let recipient1;
   let recipient2;
+  let feesReceiver1;
+  let feesReceiver2;
 
-  const fee = ethers.parseEther("1"); // Set the fee amount to 1 Ether
+  const fee = ethers.parseEther("1");
 
   before(async function () {
-    [owner, recipient1, recipient2] = await ethers.getSigners();
+    [owner, recipient1, recipient2, feesReceiver1, feesReceiver2] =
+      await ethers.getSigners();
 
     const Airdrop = await ethers.getContractFactory("Airdrop");
-    airdropContract = await Airdrop.deploy(fee);
+    airdropContract = await Airdrop.deploy(fee, feesReceiver1.address);
 
-    const ERC20Token = await ethers.getContractFactory("Token"); // Replace with your ERC20 token contract
+    const ERC20Token = await ethers.getContractFactory("Token");
     tokenContract = await ERC20Token.deploy();
 
-    // Mint some tokens to the owner for testing
     const tokenSupply = ethers.parseEther("1000");
     await tokenContract.mint(owner.address, tokenSupply);
   });
 
   it("should perform airdrop correctly", async function () {
-    // Set up the recipients and quantities
     const recipients = [recipient1.address, recipient2.address];
-    const quantities = [
-      ethers.parseEther("100"),
-      ethers.parseEther("200"),
-    ];
+    const quantities = [ethers.parseEther("100"), ethers.parseEther("200")];
 
-    // Approve the airdrop contract to spend tokens on behalf of the owner
-    await tokenContract.connect(owner).approve(airdropContract.target, ethers.parseEther("300"));
+    await tokenContract
+      .connect(owner)
+      .approve(airdropContract.target, ethers.parseEther("300"));
 
-    // Perform the airdrop
-    await airdropContract.airdrop(tokenContract.target, recipients, quantities, { value: fee });
+    await airdropContract.airdrop(
+      tokenContract.target,
+      recipients,
+      quantities,
+      { value: fee },
+    );
 
-    // Check the recipient balances
-    expect(await tokenContract.balanceOf(recipient1.address)).to.equal(quantities[0]);
-    expect(await tokenContract.balanceOf(recipient2.address)).to.equal(quantities[1]);
+    expect(await tokenContract.balanceOf(recipient1.address)).to.equal(
+      quantities[0],
+    );
+    expect(await tokenContract.balanceOf(recipient2.address)).to.equal(
+      quantities[1],
+    );
+    expect(await ethers.provider.getBalance(feesReceiver1.address)).to.equal(
+      "10001000000000000000000",
+    );
   });
 
   it("should revert when invalid input is provided", async function () {
     const recipients = [recipient1.address, recipient2.address];
     const quantities = [ethers.parseEther("100")];
 
-    // Attempt to perform the airdrop with invalid input
     await expect(
-      airdropContract.airdrop(tokenContract.target, recipients, quantities, { value: fee })
+      airdropContract.airdrop(tokenContract.target, recipients, quantities, {
+        value: fee,
+      }),
     ).to.be.revertedWith("Invalid input");
+    expect(await ethers.provider.getBalance(feesReceiver1.address)).to.equal(
+      "10001000000000000000000",
+    );
   });
 
   it("should revert when insufficient fee is sent", async function () {
     const recipients = [recipient1.address, recipient2.address];
-    const quantities = [
-      ethers.parseEther("100"),
-      ethers.parseEther("200"),
-    ];
+    const quantities = [ethers.parseEther("100"), ethers.parseEther("200")];
 
-    // Attempt to perform the airdrop with insufficient fee
     await expect(
-      airdropContract.airdrop(tokenContract.target, recipients, quantities, { value: 0 })
+      airdropContract.airdrop(tokenContract.target, recipients, quantities, {
+        value: 0,
+      }),
     ).to.be.revertedWith("Insufficient fee");
+    expect(await ethers.provider.getBalance(feesReceiver1.address)).to.equal(
+      "10001000000000000000000",
+    );
   });
 
   it("should revert when sender has insufficient balance", async function () {
     const recipients = [recipient1.address];
     const quantities = [ethers.parseEther("1000")];
 
-    // Attempt to perform the airdrop with insufficient balance
     await expect(
-      airdropContract.airdrop(tokenContract.target, recipients, quantities, { value: fee })
+      airdropContract.airdrop(tokenContract.target, recipients, quantities, {
+        value: fee,
+      }),
     ).to.be.revertedWith("Insufficient balance");
+    expect(await ethers.provider.getBalance(feesReceiver1.address)).to.equal(
+      "10001000000000000000000",
+    );
   });
 
   it("should revert when sender has insufficient allowance", async function () {
     const recipients = [recipient1.address];
     const quantities = [ethers.parseEther("100")];
 
-    // Reduce the owner's allowance for the airdrop contract
     await tokenContract.connect(owner).approve(airdropContract.target, 0);
 
-    // Attempt to perform the airdrop with insufficient allowance
     await expect(
-      airdropContract.airdrop(tokenContract.target, recipients, quantities, { value: fee })
+      airdropContract.airdrop(tokenContract.target, recipients, quantities, {
+        value: fee,
+      }),
     ).to.be.revertedWith("Insufficient allowance");
+    expect(await ethers.provider.getBalance(feesReceiver1.address)).to.equal(
+      "10001000000000000000000",
+    );
   });
-  
+
   it("should update the fee", async function () {
-    //const newFee = ethers.utils.parseEther("0.2");
     const newFee = ethers.parseEther("2");
 
-    // Ensure the current fee is 0.1 ETH
     expect(await airdropContract.fee()).to.equal(fee);
 
-    // Update the fee
     await airdropContract.updateFee(newFee);
 
-    // Ensure the fee has been updated
     expect(await airdropContract.fee()).to.equal(newFee);
+
+    const recipients = [recipient1.address, recipient2.address];
+    const quantities = [ethers.parseEther("100"), ethers.parseEther("200")];
+
+    await tokenContract
+      .connect(owner)
+      .approve(airdropContract.target, ethers.parseEther("300"));
+
+    await airdropContract.airdrop(
+      tokenContract.target,
+      recipients,
+      quantities,
+      { value: newFee },
+    );
+
+    expect(await tokenContract.balanceOf(recipient1.address)).to.equal(
+      ethers.parseEther("200"),
+    );
+    expect(await tokenContract.balanceOf(recipient2.address)).to.equal(
+      ethers.parseEther("400"),
+    );
+    expect(await ethers.provider.getBalance(feesReceiver1.address)).to.equal(
+      "10003000000000000000000",
+    );
+  });
+
+  it("should update the fee receiver", async function () {
+    await airdropContract.setFeesReceiver(feesReceiver2.address);
+
+    expect(await airdropContract.feesReceiver()).to.equal(
+      feesReceiver2.address,
+    );
+
+    const recipients = [recipient1.address, recipient2.address];
+    const quantities = [ethers.parseEther("100"), ethers.parseEther("200")];
+
+    await tokenContract
+      .connect(owner)
+      .approve(airdropContract.target, ethers.parseEther("300"));
+
+    const fees = await airdropContract.fee();
+
+    await airdropContract.airdrop(
+      tokenContract.target,
+      recipients,
+      quantities,
+      { value: fees },
+    );
+
+    expect(await ethers.provider.getBalance(feesReceiver1.address)).to.equal(
+      "10003000000000000000000",
+    );
+
+    expect(await ethers.provider.getBalance(feesReceiver2.address)).to.equal(
+      "10002000000000000000000",
+    );
   });
 });
-
